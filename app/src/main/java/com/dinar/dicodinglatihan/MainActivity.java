203c1eeab38c29b5b9fd5772b67c7b8b906a8cb2
Dinarpratnya12 <Dinarpratnya12@gmail.com>
2019-10-05 20:19:52 +0700

    tambahan biar bisa muter layar bro

--- app/src/main/java/com/dinar/dicodinglatihan/MainActivity.java
+++ app/src/main/java/com/dinar/dicodinglatihan/MainActivity.java
@@ -26,6 +26,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvResult = findViewById(R.id.tv_result);
 
         btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null){
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
     }
 
     @Override
@@ -76,5 +81,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch (NumberFormatException e) {
             return null;
         }
+    }
+
+    private static final String STATE_RESULT = "state_result";
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
     }
 }

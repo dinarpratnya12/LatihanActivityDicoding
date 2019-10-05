2a768250246a388a71fdd6a09ff14c93a6751189
Dinarpratnya12 <Dinarpratnya12@gmail.com>
2019-10-05 20:03:37 +0700

    tambahan

--- app/src/main/java/com/dinar/dicodinglatihan/MainActivity.java
+++ app/src/main/java/com/dinar/dicodinglatihan/MainActivity.java
@@ -11,9 +11,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText edtWidth, edtHeight, edtLength;
-    Button btnCalculate;
-    TextView tvResult;
+    private EditText edtWidth, edtHeight, edtLength;
+    private Button btnCalculate;
+    private TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +24,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edtLength = findViewById(R.id.edt_length);
         btnCalculate = findViewById(R.id.btn_calculate);
         tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_calculate) {
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_calculate) {
             String inputLength = edtLength.getText().toString().trim();
             String inputWidth = edtWidth.getText().toString().trim();
             String inputHeight = edtHeight.getText().toString().trim();
